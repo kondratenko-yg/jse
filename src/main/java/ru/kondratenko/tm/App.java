a0bb157e87c744c916333d2d163c65cb18e14d62
Iulia Kondratenko <kondratenko_yg@nlmk.com>
2020-07-15 18:11:00 +0300

    Добавлен выход и петля.

--- src/main/java/ru/kondratenko/tm/App.java
+++ src/main/java/ru/kondratenko/tm/App.java
@@ -3,53 +3,74 @@ package ru.kondratenko.tm;
 import ru.kondratenko.tm.constant.TerminalConst;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static ru.kondratenko.tm.constant.TerminalConst.*;
 
 public class App {
 
     public static void main(final String[] args) {
-        run(args);
         displayWelcome();
+        process();
     }
 
-    private static void run(final String[] args){
-        if(args == null) return;
-        if(args.length < 1)  return;
-        final String param = args[0];
-        switch(param){
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            case HELP: displayHelp();
-            default: displayError();
+    private static void process() {
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
+            System.out.println();
         }
     }
 
-    private static void displayError(){
-        System.out.println("Error! Unknown program argument");
-        System.exit(-1);
+    private static int run(final String param) {
+        if (param == null || param.isEmpty()) System.exit(-1);
+        switch (param) {
+            case VERSION:
+                return displayVersion();
+            case ABOUT:
+                return displayAbout();
+            case HELP:
+                return displayHelp();
+            case EXIT:
+                return displayExit();
+            default:
+                return displayError();
+        }
     }
 
-    private static void displayWelcome(){
+    private static int displayError() {
+        System.out.println("Error! Unknown program argument...");
+        return -1;
+    }
+
+    private static int displayExit() {
+        System.out.println("Terminate console application...");
+        return 0;
+    }
+
+    private static void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static void displayHelp(){
+    private static int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
-        System.exit(0);
+        System.out.println("exit - Terminate console application.");
+        return 0;
     }
 
-    private static void displayVersion(){
+    private static int displayVersion() {
         System.out.println("1.0.0");
-        System.exit(0);
+        return 0;
     }
 
-    private static void displayAbout(){
+    private static int displayAbout() {
         System.out.println("Kondratenko Iulia");
         System.out.println("kondratenko_yg@nlmk.com");
-        System.exit(0);
+        return 0;
     }
 
 }
